2cfb2c8476412747b9d7b1d75595761ce3dd6e62
DanLouble <128084601+DanLouble@users.noreply.github.com>
2024-09-14 15:21:16 +0100

    Add files via upload

--- prime numbers.py
+++ prime numbers.py
@@ -1,21 +1,24 @@
-import numpy
-
 count = 0
 
 primenumbers = []
 
 numTester = int(input("enter a number for all the prime up to the number "))
 #if not numTester // 2 == 0 and not numTester // 3 == 0 and not numTester // 5 == 0:
-    
 
-while count <= numTester:
-    #if count == 2:
-        #print(count)
-    if not count % 2 == 0 and not count % 3 == 0 and not count % 5 == 0:
+primes =[]
+
+for count in range(2, numTester + 1):
+    prime = True
+    for eachPrime in primes:
+        if count % eachPrime == 0:
+            prime = False
+            break
+    if prime:
         print(count)
-        primenumbers.append(count)
+        primes.append(count)
         
         #print(numTester
 
-    count+=1
+    
 #print(primenumbers)
+
